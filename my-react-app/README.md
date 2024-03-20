ccff0fdc151f072c9671f16c561b99f61a80cf02
Nicolas Prothero <nprothero0917@gmail.comgit config --global user.name Nicolas Prothero>
2024-03-20 13:35:35 -0400

    first init, and making the base layout for the frontend, and create TaskBox to allow backend to create tasks

---
+++ my-react-app/README.md
@@ -0,0 +1,1 @@
+# PRODUCTITY PRO
---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
