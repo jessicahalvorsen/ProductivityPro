0c6519b179e173bea9cad08f10dd6f68df0d54d4
Jessica Halvorsen <j.halvorsen@ufl.edu>
2024-03-21 13:17:46 -0400

    Added Code Needed-Not working
    - for some reason it won't connect with port 4000. Not sure if it is a vite thing?

--- my-react-app/src/components/TaskBox.jsx
+++ my-react-app/src/components/TaskBox.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+//import React from 'react';
 
-function TaskBox({ title, tag }) {
+const TaskBox = ({task}) => {
     return (
         <div className="p-4 rounded bg-white w-40 h-28 flex flex-col justify-between">
-            <h1 className="text-xs font-bold w-16 text-left">{title}</h1>
-            <p className="text-sm text-red-400 self-end">{tag}</p>
+            <h1 className="text-xs font-bold w-16 text-left">{task.title}</h1>
+            <p className="text-sm text-red-400 self-end">{task.description}</p>
         </div>
-    );
+    )
 }
 
 export default TaskBox;
