40bb3efad56e3b68325b2742fac620ffbff4dd89
Jessica Halvorsen <j.halvorsen@ufl.edu>
2024-03-24 21:44:32 -0400

    Adding New Task Functionality
    - added ability to add a new task to a user's page
    - tasks are private to each user
    - added first and last names when creating user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,14 +25,14 @@ const loginUser = async (req, res) => {
 
 // sign up user
 const signupUser = async (req, res) => {
-    const {email, password} = req.body
+    const {firstName, lastName, email, password} = req.body
     try {
-        const user = await User.signup(email, password)
+        const user = await User.signup(firstName, lastName, email, password)
 
         // create a token 
         const token = createToken(user._id)
 
-        res.status(200).json({email, token})
+        res.status(200).json({firstName, lastName, email, token})
     } catch(error) {
         res.status(400).json({error: error.message})
     }
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { AuthContextProvider } from './context/AuthContext'
+import { TaskContextProvider } from './context/taskContext.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <TaskContextProvider>
+      
+        <App />
+      </TaskContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 )
